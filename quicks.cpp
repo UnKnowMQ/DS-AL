a8e518f05ba81d4ea73c96956cba85e77372c389
UnKnowMQ <118916191+UnKnowMQ@users.noreply.github.com>
2024-02-22 23:57:45 +0700

    Update quicks.cpp

--- quicks.cpp
+++ quicks.cpp
@@ -10,9 +10,11 @@ a = b;
 }
 int part(vector<int> a,int low, int high)
 {
+	//right must be the number before privot( privot is the last number)
 		int left = low, right  = high-1	, prot = a[high];
 	while( true)
 	{
+		// choose 2 number that need to change direction #1st
 		while(left <= right && a[left] < prot) 
 		{
 		
@@ -23,26 +25,33 @@ int part(vector<int> a,int low, int high)
 		
 		right--;
 	}
+		
+		//end of changing directions, ready to change privot to its correct direction 
 		if(left >=  right) 
 		
 		{
 		
 		break;
 	}
+		// change direction of 2 number in #1st
 		swap(a[left],a[right]);
+		// need to change pointer to 2 number after
 		left++;
 		right--;
 		
 	}
+	// change privot to its correct direction
 	swap(a[left],a[high]);
+	//Finish 1 turn of recursion and return the dir of the correct 1 number, ready to divide and solve
 	return left;
 }
 void quicksort(vector<int> a,int low,int high)
 {
 	if(low < high)
 	{
-	
+	// dividing array 
 	int pi = part(a,low,high);
+	//solve the 2 divided array 
 	quicksort(a,low , pi - 1);
 	quicksort(a, pi +  1,high);
 }
@@ -65,9 +74,6 @@ int main()
 	{
 		cout<< tryan[i] << " ";
 	}
-	int c = 4;
-	int d = 5;
-	swap(c,d);
-	cout<< " \n" << c << " " << d;
+
 	
 }
